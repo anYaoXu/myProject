4486be2ecb9ab4a8cf95125e8785dfac0649774f
xuyanru <3131895796@qq.com>
2019-04-24 21:59:18 +0800

    table header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myProject';
+  header = [
+    {
+      list: [
+        {
+          title: '年级',
+          row: 2,
+          isSort: true,
+          keyName: 'name'
+        },
+        {
+          title: '平均分',
+          col: 3
+        },
+        {
+          title: '人数',
+          col: 3
+        }
+      ]
+    },
+    {
+      list: [
+        {
+          title: '一班'
+        },
+        {
+          title: '二班'
+        },
+        {
+          title: '三班'
+        },
+        {
+          title: '一班'
+        },
+        {
+          title: '二班'
+        },
+        {
+          title: '三班',
+        }
+      ]
+    }
+  ];
+  tableData = [
+    {
+      a: '一年级',
+      b: 10,
+      c: 20,
+      d: 30,
+      e: 44,
+      f: 55,
+      g: 66
+    },
+    {
+      a: '二年级',
+      b: 10,
+      c: 20,
+      d: 30,
+      e: 44,
+      f: 55,
+      g: 66
+    },
+    {
+      a: '三年级',
+      b: 10,
+      c: 20,
+      d: 30,
+      e: 44,
+      f: 55,
+      g: 66
+    }
+  ]
 }
